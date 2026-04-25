b6ae967dbab727d3eee61643371b48ba798da4bf
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-04-25 17:59:47 +0300

    feat: project form added

---
+++ js/Project.js
@@ -0,0 +1,25 @@
+class Project {
+  static id_form_wrapper = "right_form__project";
+
+  static cancel() {
+    const DIV = document.getElementById(this.id_form_wrapper);
+
+    if (!DIV) {
+      console.error(`Node not found #${this.id_form_wrapper}`);
+      alert(`Node not found #${this.id_form_wrapper}`);
+    }
+
+    DIV.classList.add("project_add_form--close");
+  }
+
+  static open() {
+    const DIV = document.getElementById(this.id_form_wrapper);
+
+    if (!DIV) {
+      console.error(`Node not found #${this.id_form_wrapper}`);
+      alert(`Node not found #${this.id_form_wrapper}`);
+    }
+
+    DIV.classList.remove("project_add_form--close");
+  }
+}
